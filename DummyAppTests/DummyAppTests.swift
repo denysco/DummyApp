489284461876d76cd2305f2f2c9b8b9816e6ca9a
Denys <denys.gar.dev@gmail.com>
2022-11-24 13:10:24 -0500

    add dummy project

---
+++ DummyAppTests/DummyAppTests.swift
@@ -0,0 +1,15 @@
+//
+//  DummyAppTests.swift
+//  DummyAppTests
+//
+//  Created by Denys on 11/21/22.
+//
+
+import XCTest
+@testable import DummyApp
+
+final class DummyAppTests: XCTestCase {
+    func test() {
+        
+    }
+}
